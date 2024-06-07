831f43e543f073daf7fa5a9eac3c1c94fd33a719
Shreejwal Kaphle <shreejwal.kaphle@compliancequest.com>
2024-06-07 11:59:38 +0545

    [commit-3] | property Binding: bind the property of input element  ie placeholder property with the data present in the component.

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -3,8 +3,7 @@ import { Component } from "@angular/core";
 @Component({
   selector:'app-header',
   templateUrl:'./header.component.html'
-  // template:'<h1>header</h1>'
 })
 export class HeaderComponent{
-
+  searchText:String = "enter your searchText"
 }
