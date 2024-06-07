58dac4a18b4e69accda4953729aaf104e819703d
Shreejwal Kaphle <shreejwal.kaphle@compliancequest.com>
2024-06-07 14:27:54 +0545

    [commit-8] | parent to child Data passing: using property binding: parent and child components are made and from parent the data is sent to the child components. introduction of @Input() decorator.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,11 @@ import { BrowserModule } from "@angular/platform-browser";
 import { AppComponent } from "./app.component";
 import { HeaderComponent } from "./header/header.component";
 import { FormsModule } from "@angular/forms";
+import { Parent } from "./parent/parent.component";
+import { Child } from "./child/child.component";
 
 @NgModule({
-  declarations:[AppComponent,HeaderComponent],
+  declarations:[AppComponent,HeaderComponent,Parent,Child],
   imports:[BrowserModule,FormsModule],
   providers:[],
   bootstrap:[AppComponent]
---
+++ src/app/parent/parent.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector:"parent-component",
+  templateUrl:"./parent.component.html"
+})
+export class Parent{
+  sender="shreejwal kaphle";
+}
